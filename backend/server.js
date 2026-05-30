b2dcb28d71206263c54cb1ea69d302c7c39dc8ba
Akhand827 <akhandgupta2001@gmail.com>
2026-05-30 20:41:28 +0530

    3- payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -19,7 +20,8 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); //to parse incoming JSON data (req.body)
+app.use(express.json({ limit: "5mb" })); //to parse incoming JSON data (req.body) and set a limit for the request body size to 5MB to prevent abuse
+//limit shouldn't be too small to allow image uploads, but also not too large to prevent DOS attacks with large payloads
 app.use(express.urlencoded({ extended: true })); //to parse URL-encoded data (form submissions)
 
 app.use(cookieParser()); //to parse cookies from incoming requests
